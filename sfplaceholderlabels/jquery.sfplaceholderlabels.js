26dbd61b854957ee8d2099fc6dc5647b8def5134
Tim Williamson <timw255@gmail.com>
2012-01-27 09:19:28 -0500

    Bug fix

--- sfplaceholderlabels/jquery.sfplaceholderlabels.js
+++ sfplaceholderlabels/jquery.sfplaceholderlabels.js
@@ -1,15 +1,15 @@
 /*!
- * sfPlaceholderLabels - jQuery Content Placeholder Labels for Sitefinity 4.x
- *
- * Copyright (c) 2012 Tim Williamson
- *
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * Version : 1.1.0
- * Released: Thursday 26th January, 2012
- */
+* sfPlaceholderLabels - jQuery Content Placeholder Labels for Sitefinity 4.x
+*
+* Copyright (c) 2012 Tim Williamson
+*
+* Dual licensed under the MIT or GPL Version 2 licenses.
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl-2.0.html
+*
+* Version : 1.1.1
+* Released: Friday 27th January, 2012
+*/
 (function ($) {
     $.fn.sfplaceholderlabels = function (options) {
 
@@ -48,35 +48,49 @@
                 }
 
                 if (options.initVisible == false) {
+                    $radDockZone.attr('title', $radDockZone.data('title'));
                     $radDockZone.find('.placeholderMeta').toggle();
                 }
             }
         });
 
         function contractSection() {
+
             var $radDockZone = $(this).closest('.RadDockZone');
             $radDockZone.data('oldHeight', $radDockZone.innerHeight());
 
             $radDockZone.addClass('sfplCollapsed');
             $radDockZone.find('.sfplOverlay').toggle();
-            $radDockZone.attr('style', 'min-height:23px !important;min-width:10px').stop().animate({ height: '23px' }, 250, function () {
+
+            if ($radDockZone.hasClass('zeDockZoneEmpty')) {
+                $radDockZone.attr('style', 'min-height:23px !important;min-width:10px')
+            }
+
+            $radDockZone.stop().animate({ height: '23px' }, 250, function () {
                 $(this).find('.toggle.collapse').removeClass('collapse').addClass('collapsed').unbind('click.sfPL').bind('click.sfPL', expandSection);
             });
         }
 
         function expandSection() {
+
             var $radDockZone = $(this).closest('.RadDockZone');
 
             $radDockZone.stop().animate({ height: $radDockZone.data('oldHeight') }, 500, function () {
                 $radDockZone.find('.sfplOverlay').toggle();
                 $radDockZone.removeClass('sfplCollapsed');
-                $radDockZone.attr('style', 'min-height:' + $radDockZone.data('oldHeight') + 'px !important;min-width:10px;');
+
+                if ($radDockZone.hasClass('zeDockZoneEmpty')) {
+                    $radDockZone.attr('style', 'min-height:' + $radDockZone.data('oldHeight') + 'px !important;min-width:10px;');
+                }
+
                 $(this).find('.toggle.collapsed').removeClass('collapsed').addClass('collapse').unbind('click.sfPL').bind('click.sfPL', contractSection);
             });
         }
 
         function listener(event) {
+
             if (event.keyCode == 90) {
+
                 if (options.expandOnHide == true) {
                     $('.toggle.collapsed').each(expandSection);
                 }
